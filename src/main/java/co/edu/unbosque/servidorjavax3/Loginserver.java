fb19d56aebf10bdcfac5903b802aaa0c641b71a3
jpizar@unbosque.edu.co <jpizarodrugiez@gmail.com>
2022-03-30 20:24:29 -0500

    base del proyecto

---
+++ src/main/java/co/edu/unbosque/servidorjavax3/Loginserver.java
@@ -0,0 +1,53 @@
+package co.edu.unbosque.servidorjavax3;
+
+import co.edu.unbosque.servidorjavax3.Dtos.User;
+import co.edu.unbosque.servidorjavax3.services.UserService;
+
+import java.io.*;
+import java.util.List;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+
+@WebServlet(name = "login", value = "/login")
+public class Loginserver extends HttpServlet {
+    private String message;
+
+    public void init() {
+        message = "Hello World! :V";
+    }
+
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("text/html");
+        String username=request.getParameter("username");
+        String password=request.getParameter("password");
+
+        List<User> Users= new UserService().getUsers().get();
+          User userfound =Users.stream()
+                .filter(user -> username.equals(user.getUsername()) && password.equals(user.getPassword()))
+                .findFirst().get();
+        if(username.equals(userfound.getUsername()) && password.equals(userfound.getPassword())){
+            /*message= username + "welcome";
+            PrintWriter out = response.getWriter();
+            out.println("<html><body>");
+            out.println("<h1>" + message + "</h1>");
+            out.println("</body></html>");*/
+            request.setAttribute("role",userfound.getRole());
+            RequestDispatcher dispatcher=request.getRequestDispatcher("./home.jsp");
+            try {
+                dispatcher.forward(request, response);
+            } catch (ServletException e) {
+                e.printStackTrace();
+            }
+        }else{
+            System.out.println("No autorizado viejo :c");
+            response.sendRedirect("./401.html");
+        }
+        // Hello
+
+    }
+
+    public void destroy() {
+    }
+}
---
+++ src/main/java/co/edu/unbosque/servidorjavax3/services/UserService.java
@@ -0,0 +1,47 @@
+package co.edu.unbosque.servidorjavax3.services;
+
+import co.edu.unbosque.servidorjavax3.Dtos.User;
+import com.opencsv.bean.CsvToBean;
+import com.opencsv.bean.CsvToBeanBuilder;
+import com.opencsv.bean.HeaderColumnNameMappingStrategy;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+import java.util.Optional;
+
+public class UserService {
+    public Optional<List<User>> getUsers() throws IOException {
+
+        List<User> users;
+
+        try (InputStream is = UserService.class.getClassLoader()
+                .getResourceAsStream("users.csv")) {
+
+            if (is == null) {
+                return Optional.empty();
+            }
+
+            HeaderColumnNameMappingStrategy<User> strategy = new HeaderColumnNameMappingStrategy<>();
+            strategy.setType(User.class);
+
+            try (BufferedReader br = new BufferedReader(new InputStreamReader(is, StandardCharsets.UTF_8))) {
+
+                CsvToBean<User> csvToBean = new CsvToBeanBuilder<User>(br)
+                        .withType(User.class)
+                        .withMappingStrategy(strategy)
+                        .withIgnoreLeadingWhiteSpace(true)
+                        .build();
+
+                users = csvToBean.parse();
+            }
+        }
+
+        return Optional.of(users);
+    }
+
+
+}
