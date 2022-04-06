e80e35ceb02557073f8b72a3d209a886b8e22787
jpizar@unbosque.edu.co <jpizarodrugiez@gmail.com>
2022-04-05 20:36:42 -0500

    cambios para el multiservlets y forma para escribir en csv

--- src/main/java/co/edu/unbosque/servidorjavax3/services/listfiles.java
+++ src/main/java/co/edu/unbosque/servidorjavax3/services/listfiles.java
@@ -3,10 +3,10 @@ package co.edu.unbosque.servidorjavax3.services;
 
 import com.google.gson.Gson;
 
-import jakarta.servlet.annotation.WebServlet;
-import jakarta.servlet.http.HttpServlet;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 
 import java.io.File;
