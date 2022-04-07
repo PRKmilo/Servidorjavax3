456b88719f5b66baf9617bce01a29ef926df2b6e
jpizar@unbosque.edu.co <jpizarodrugiez@gmail.com>
2022-04-07 18:35:38 -0500

    cambios de pieza

--- src/main/java/co/edu/unbosque/servidorjavax3/Dtos/Pieza.java
+++ src/main/java/co/edu/unbosque/servidorjavax3/Dtos/Pieza.java
@@ -1,5 +1,50 @@
 package co.edu.unbosque.servidorjavax3.Dtos;
 
-public class Pieza {
+import com.opencsv.bean.CsvBindByName;
 
+public class Pieza {
+    @CsvBindByName
+    private String titulo;
+
+    @CsvBindByName
+    private String precio;
+
+    @CsvBindByName
+    private String user;
+
+    @CsvBindByName
+    private String img;
+
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getPrecio() {
+        return precio;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public String getImg() {
+        return img;
+    }
+
+    public void setImg(String img) {
+        this.img = img;
+    }
+
+    public void setPrecio(String precio) {
+        this.precio = precio;
+    }
 }
